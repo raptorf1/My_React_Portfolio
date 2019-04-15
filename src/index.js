58d140a916d66b586ff9d285383ef0f84931bacd
raptorf1 <raptor_f1@hotmail.com>
2019-04-15 15:06:31 +0200

    Adds styling to all components using tailwind and classNames

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom"
 import Header from "./Header"
 import Hello from "./Hello"
 import Footer from "./Footer"
+import "./css/tailwind.css"
 
 const App = () => {
-    return <div>
-                <Header />
-                <Hello />
+    return <div className="min-h-screen">
+                <div className="flex flex-col w-full fixed pin-1 pin-y">
+                    <Header />
+                    <div className="px-4 pt-4 flex-1 overflow-y-scrol">
+                        <Hello />
+                    </div>
                 <Footer />
+                </div>
             </div>
 }
 
