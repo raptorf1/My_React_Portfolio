636bcf372af8413f665d9802b57c958c8d47d90b
raptorf1 <raptor_f1@hotmail.com>
2019-04-16 09:42:54 +0200

    Modifies Projects.jsx to a component class and adds a list of projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,53 @@
-import React from "react"
+import React, { Component } from "react"
 
-const Projects = () => {
-    return (
-        <div>
-            <h1 className="content-text">My Projects</h1>
-            <p className="content-text">Ipsum kai ta loipa</p>
-        </div>
-    )
-}
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [
+                {
+                    "id": 1,
+                    "name": "My First Website"
+                },
+                {
+                    "id": 2,
+                    "name": "My Address Book"
+                },
+                {
+                    "id": 3,
+                    "name": "FizzBuzz"
+                },
+                {
+                    "id": 4,
+                    "name": "Library Challenge"
+                }
+            ]
+        };
+    }
+
+    render () {
+        const projects = this.state.projects
+        let projectsList
+
+        if (projects.length > 0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <h3 className="content-text">
+                            {project.name}
+                        </h3>
+                    </div>
+                )
+            })
+        }
+
+        return(
+            <div>
+                <h1 className="content-text">My Projects</h1>
+                {projectsList}
+            </div>
+        )
+    }
+};
 
 export default Projects
