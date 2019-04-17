5bebd686b0cc4976ab40623e3e02dc2fb1430f81
raptorf1 <raptor_f1@hotmail.com>
2019-04-17 14:05:32 +0200

    Adds Netlify's contact form to the Contact component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,17 +2,24 @@ import React from "react"
 
 const Contact = () => {
     return (
-        <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-        <p class="hidden">
-            <label>Don’t fill this out if you're human: <input name="bot-field" /></label></p>
-        <p>
-            <label>Email: <input type="text" name="email" /></label></p>
-        <p>
-            <label>Message: <textarea name="message"></textarea></label></p>
-        <p>
-            <button type="submit">Send</button></p>
-        </form>
-    )
+        <div class="w-full max-w-xs">
+            <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <p class="hidden"><label>Don’t fill this out if you're human: <input name="bot-field" /></label></p>
+                    <p></p>
+                    <label class="block text-grey-darker text-sm font-bold mb-2">E-Mail</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="email" />
+                </div>
+                <div class="mb-6">
+                    <label class="block text-grey-darker text-sm font-bold mb-2">Message</label>
+                    <textarea class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" name="message" />
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Send</button>
+                    </div>
+            </form>        
+        </div>
+            )
 }
 
 export default Contact
