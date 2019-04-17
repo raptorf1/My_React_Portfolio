80aa928f6cf972cc0d7de98be2f9064c421cb1d5
raptorf1 <raptor_f1@hotmail.com>
2019-04-17 21:09:14 +0200

    Adds GitHub clickable logo in the Footer component, edits styling of Footer component.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,8 +5,9 @@ const Footer = () => {
         <div className="footer-wrapper">
             <div className="footer-content">
                 <img src="https://www.codewars.com/users/raptorf1/badges/small" alt="Codewars Badge" ></img>
-                <a href="https://reactjs.org/" target="_blank"><img src="./src/data/reactLogo.png" height="40" width="40" alt="React Logo"></img></a>
-                <a href="https://twitter.com/raptor__f1" target="_blank"><img src="./src/data/twitterLogo.png" height="40" width="40" alt="Twitter Logo"></img></a>
+                <a href="https://github.com/raptorf1" target="_blank"><img src="./src/data/github.png" width="100" alt="GitHub Logo"></img></a>
+                <a href="https://twitter.com/raptor__f1" target="_blank"><img src="./src/data/twitterLogo.png" height="30" width="30" alt="Twitter Logo"></img></a>
+                <a href="https://reactjs.org/" target="_blank"><img src="./src/data/reactLogo.png" height="30" width="30" alt="React Logo"></img></a>
             </div>
         </div>
     )
