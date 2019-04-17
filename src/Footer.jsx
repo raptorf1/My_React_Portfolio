cc5150ad90d5112ed82c38dc13b06a993b5ff807
raptorf1 <raptor_f1@hotmail.com>
2019-04-17 13:26:06 +0200

    Edits Footer and adds a twitter logo linking to my Twitter account

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
         <div className="footer-wrapper">
             <div className="footer-content">
                 <img src="https://www.codewars.com/users/raptorf1/badges/small" alt="Codewars Badge" ></img>
-                <a href="https://reactjs.org/"><img src="./src/data/reactLogo.png" height="40" width="40" alt="React Logo"></img></a>
+                <a href="https://reactjs.org/" target="_blank"><img src="./src/data/reactLogo.png" height="40" width="40" alt="React Logo"></img></a>
+                <a href="https://twitter.com/raptor__f1" target="_blank"><img src="./src/data/twitterLogo.png" height="40" width="40" alt="Twitter Logo"></img></a>
             </div>
         </div>
     )
